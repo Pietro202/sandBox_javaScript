cf5bcc4c34920f68a27102d0f6e83db6ef6058c5
Pietrothelly Paolo <pietrothelly@ibm.com>
2023-04-28 17:21:39 -0300

    Alterando estilos com js

---
+++ javascript/Javascript 2/13- Manipilando Estilos com JS/index.js
@@ -0,0 +1,20 @@
+function darkTheme(){
+    document.body.style.color = '#f1f5f9'
+    document.body.style.backgroundColor = '#212529'  
+}
+
+function lightTheme(){
+    document.body.style.color = '#212529' 
+    document.body.style.backgroundColor = '#f1f5f9' 
+}
+
+function switchTheme(){
+    document.body.classList.toggle('is-dark')
+    document.body.classList.toggle('is-light')
+
+}
+
+document.getElementById('darkBtn').addEventListener('click', darkTheme)
+document.getElementById('lightBtn').addEventListener('click', lightTheme)
+document.getElementById('switchBtn').addEventListener('click', switchTheme)
+
